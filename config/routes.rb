1476de11fcfa15b9c51debb55e737dc5f3f0eeeb
Daniel Berzon <daniel@ocasta.com>
2018-09-28 13:56:11 +0100

    adding home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :responses
   resources :participants
   root to: 'home#temproot'
+  get 'home' => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
