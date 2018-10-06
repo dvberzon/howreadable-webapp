7a7b62047089f97f0d615a2da74d3fe2c610045c
Daniel van Berzon <dvberzon@gmail.com>
2018-10-06 18:40:12 +0100

    forgot routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  resources :responses
+  
+  namespace :admin do
+    root to: 'participants#index'
+    resources :participants, only: [:index, :show, :destroy]
+    resources :responses, only: [:index]
+  end
+
   resources :participants do
     resources :responses
   end
+
   root to: 'home#temproot'
   get 'home' => 'home#index'
   get 'thankyou' => 'home#thankyou', as: :thankyou
+  get 'start/:lang' => 'participants#new', as: :start
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
