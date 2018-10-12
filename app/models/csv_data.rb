3574413b88899ae769d870255f2d08bf016c9276
Daniel van Berzon <dvberzon@gmail.com>
2018-10-12 16:41:02 +0100

    fix csv headers

--- app/models/csv_data.rb
+++ app/models/csv_data.rb
@@ -20,11 +20,10 @@ class CsvData
         'Client h',
         'Response index',
         'Test case',
-        'Index',
         'Example',
-        'Participant id',
         'Responded ms',
         'Given answer',
+        'Answer correct?',
         'Readable score',
         'Comments'
       ]
@@ -32,23 +31,23 @@ class CsvData
         if(response.given_answer)
           participant = response.participant
           row = [
-            participant.id,
-            participant.main_language,
-            participant.years_programming,
-            participant.language_choice,
-            participant.created_at,
-            participant.agent,
-            participant.ip,
-            participant.client_w,
-            participant.client_h,
-            response.index,
-            response.test_case,
-            response.example,
-            response.responded_ms,
-            response.given_answer,
-            response.correct? ? '1' : '0',
-            response.readable_score,
-            response.comments,
+            participant.id, 
+            participant.main_language, 
+            participant.years_programming, 
+            participant.language_choice, 
+            participant.created_at, 
+            participant.agent, 
+            participant.ip, 
+            participant.client_w, 
+            participant.client_h, 
+            response.index, 
+            response.test_case, 
+            response.example, 
+            response.responded_ms, 
+            response.given_answer, 
+            response.correct? ? '1' : '0', 
+            response.readable_score, 
+            response.comments, 
           ]
           csv << row
         end
