23e94263a50e5cabfd49d202c902b0c11f181e35
Daniel van Berzon <dvberzon@gmail.com>
2019-10-05 20:45:16 +0100

    fixing admin

--- app/models/csv_data.rb
+++ app/models/csv_data.rb
@@ -20,34 +20,32 @@ class CsvData
         'Client h',
         'Response index',
         'Test case',
-        'Example',
+        'Excercise',
+        'Pattern',
         'Responded ms',
         'Given answer',
         'Answer correct?',
-        'Readable score',
-        'Comments'
       ]
       Response.includes(:participant).order(:id).find_each do |response|
         if(response.given_answer)
           participant = response.participant
           row = [
-            participant.id, 
-            participant.main_language, 
-            participant.years_programming, 
-            participant.language_choice, 
-            participant.created_at, 
-            participant.agent, 
-            participant.ip, 
-            participant.client_w, 
-            participant.client_h, 
-            response.index, 
-            response.test_case, 
-            response.example, 
-            response.responded_ms, 
-            response.given_answer, 
-            response.correct? ? '1' : '0', 
-            response.readable_score, 
-            response.comments, 
+            participant.id,
+            participant.main_language,
+            participant.years_programming,
+            participant.language_choice,
+            participant.created_at,
+            participant.agent,
+            participant.ip,
+            participant.client_w,
+            participant.client_h,
+            response.index,
+            response.test_case,
+            response.exercise_id,
+            response.pattern,
+            response.responded_ms,
+            response.given_answer,
+            response.correct? ? '1' : '0',
           ]
           csv << row
         end
@@ -71,14 +69,14 @@ class CsvData
       ]
       Participant.includes(:responses).order(:id).find_each do |participant|
         row = [
-          participant.id, 
-          participant.main_language, 
-          participant.years_programming, 
-          participant.language_choice, 
-          participant.created_at, 
-          participant.agent, 
-          participant.ip, 
-          participant.client_w, 
+          participant.id,
+          participant.main_language,
+          participant.years_programming,
+          participant.language_choice,
+          participant.created_at,
+          participant.agent,
+          participant.ip,
+          participant.client_w,
           participant.client_h,
           participant.responses.count {|r| r.given_answer }
         ]
