12b311f7860cfddb3405074c5fd87ccd57624f67
Daniel van Berzon <dvberzon@gmail.com>
2019-10-09 22:25:30 +0100

    fix for ssl issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # stop forgery protection for url (ssl issues)
+  config.action_controller.allow_forgery_protection = false
 end
