237f3806c83affc2a5163260a86ff7374de95ff7
Daniel van Berzon <dvberzon@gmail.com>
2019-10-18 16:00:21 +0100

    adding stats histograms

--- app/models/stats.rb
+++ app/models/stats.rb
@@ -27,11 +27,12 @@ class Stats
       test_case[:participants] += 1
     end
     pattern = (test_case[:patterns][response.pattern] ||= {})
-    pattern[:overall] ||= ({
+    pattern['overall'] ||= ({
       total: 0,
       total_ms: 0,
       total_correct: 0,
       total_readable: 0,
+      correct_times: []
     })
     exercise = response.exercise_id
     pattern[exercise] ||= ({
@@ -40,12 +41,15 @@ class Stats
       total_correct: 0,
       total_readable: 0
     })
-    [pattern[:overall], pattern[exercise]].each do |stat|
+    [pattern['overall'], pattern[exercise]].each do |stat|
       stat[:total] += 1
       if response.correct?
         stat[:total_ms] += (response.responded_ms || 0)
         stat[:total_correct] += 1
       end
+    end
+    if response.correct?
+      pattern['overall'][:correct_times].push response.responded_ms
     end
   end
 
